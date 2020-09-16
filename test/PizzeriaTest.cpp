a041d7700a7e08931519f3cfd1057b1e66cb001c
MaciejSalamonski <macieksalamonski@gmail.com>
2020-09-17 01:18:12 +0200

    homework - pizzas

--- test/PizzeriaTest.cpp
+++ test/PizzeriaTest.cpp
@@ -3,16 +3,16 @@
 #include "Funghi.hpp"
 #include "Margherita.hpp"
 #include "Pizzeria.hpp"
-#include "mocks/DummyTimer.hpp"
 #include "mocks/PizzaMock.hpp"
+#include "mocks/TimerMock.hpp"
 
 using namespace std;
 using namespace ::testing;
 
 struct PizzeriaTest : public ::testing::Test {
 public:
-    DummyTimer dt;
-    Pizzeria pizzeria = Pizzeria("dummyName", dt);
+    StrictMock<TimerMock> timerMock{};
+    Pizzeria pizzeria = Pizzeria("dummyName", timerMock);
 };
 
 TEST_F(PizzeriaTest, priceForMargherita25AndFunghi30ShouldBe55) {
@@ -30,6 +30,7 @@ TEST_F(PizzeriaTest, priceForMargherita25AndFunghi30ShouldBe55) {
 TEST_F(PizzeriaTest, bakeDummyPizza) {
     // Given
     Pizzas pizzas = {new PizzaDummy{}};
+    EXPECT_CALL(timerMock, sleep_for).Times(1);
 
     // When
     auto orderId = pizzeria.makeOrder(pizzas);
@@ -39,6 +40,7 @@ TEST_F(PizzeriaTest, bakeDummyPizza) {
 TEST_F(PizzeriaTest, completeOrderWithStubPizza) {
     // Given
     Pizzas pizzas = {new PizzaStub{"STUB"}};
+    EXPECT_CALL(timerMock, sleep_for).Times(1);
 
     // When
     auto orderId = pizzeria.makeOrder(pizzas);
@@ -50,12 +52,48 @@ TEST_F(PizzeriaTest, calculatePriceForPizzaMock) {
     // Given
     PizzaMock mock{};
     Pizzas pizzas = {&mock};
-    EXPECT_CALL(mock, getPrice()).WillOnce(Return(40.0));
+    constexpr double mockPizzaPrice = 40.0;
+    EXPECT_CALL(mock, getPrice()).WillOnce(Return(mockPizzaPrice));
 
     // When
     auto orderId = pizzeria.makeOrder(pizzas);
     auto price = pizzeria.calculatePrice(orderId);
 
     // Then
-    ASSERT_EQ(40, price);
+    ASSERT_EQ(mockPizzaPrice, price);
+}
+
+TEST_F(PizzeriaTest, mainSimulationCase) {
+    //Given
+    constexpr double strictPizzaPrice = 5.0;
+    constexpr double nicePizzaPrice = 10.0;
+    constexpr char strictPizzaName[] = "strictPizza";
+    constexpr char nicePizzaName[] = "nicePizza";
+    constexpr minutes strictPizzaTime{5};
+    constexpr minutes nicePizzaTime{2};
+    PizzaStub stubPizza{"stub"};
+    StrictMock<PizzaMock> strictMockPizza{};
+    NiceMock<PizzaMock> niceMockPizza{};
+    Pizzas pizzas = {&stubPizza, &strictMockPizza, &niceMockPizza};
+
+    EXPECT_CALL(strictMockPizza, getName()).WillOnce(Return(strictPizzaName));
+    EXPECT_CALL(strictMockPizza, getPrice()).WillOnce(Return(strictPizzaPrice));
+    EXPECT_CALL(strictMockPizza, getBakingTime()).WillOnce(Return(strictPizzaTime));
+
+    EXPECT_CALL(niceMockPizza, getName()).WillOnce(Return(nicePizzaName));
+    EXPECT_CALL(niceMockPizza, getPrice()).WillOnce(Return(nicePizzaPrice));
+    EXPECT_CALL(niceMockPizza, getBakingTime()).WillOnce(Return(nicePizzaTime));
+
+    EXPECT_CALL(timerMock, sleep_for(strictPizzaTime)).Times(1);
+    EXPECT_CALL(timerMock, sleep_for(nicePizzaTime)).Times(1);
+    EXPECT_CALL(timerMock, sleep_for(stubPizza.getBakingTime())).Times(1);
+
+    //When
+    auto orderId = pizzeria.makeOrder(pizzas);
+    auto price = pizzeria.calculatePrice(orderId);
+    pizzeria.bakePizzas(orderId);
+    pizzeria.completeOrder(orderId);
+
+    //Then
+    ASSERT_EQ(strictPizzaPrice + nicePizzaPrice, price);
 }
--- test/mocks/DummyTimer.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "Timer.hpp"
-
-class DummyTimer : public Timer {
-public:
-    void sleep_for(minutes duration) override {}
-};
